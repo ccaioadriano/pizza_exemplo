8a4ac53a4a21ccc2dd2f3c2840185680cebbc968
Caio Adriano <caiobhadriano@gmail.com>
2025-03-12 01:06:34 -0300

    installing breeze

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-//rotas centrais
-foreach (config('tenancy.central_domains') as $domain) {
-    Route::domain($domain)->group(function () {
-        Route::get('/', function () {
-            return view('welcome');
-        });
-    });
-}
+Route::get('/', function () {
+    return ['Laravel' => app()->version()];
+});
+
+require __DIR__.'/auth.php';
